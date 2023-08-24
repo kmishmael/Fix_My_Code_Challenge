fae9b2e7f0b38450e59d22a0c33273248f698de6
kmishmael <kmishmael@gmail.com>
2023-08-24 13:10:46 +0300

    ruby on railway :)

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -11,8 +11,6 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-
-    puts "got " + i_arg.to_s
     
     # insert result at the right position
     is_inserted = false
@@ -22,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
